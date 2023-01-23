c0669890aedf9c403b93885f3c8ff5c02c7a5a19
Sikha Kowshal <koushalsikha4@gmail.com>
2023-01-23 12:05:35 +0000

    final-code

--- src/components/CoinToss/index.js
+++ src/components/CoinToss/index.js
@@ -6,19 +6,19 @@ class CoinToss extends Component {
   state = {result: 'heads', headsCount: 0, tailsCount: 0, totalCount: 0}
 
   tossCoin = () => {
-    const tossResult = Math.ceil(Math.random() * 2)
+    const tossResult = Math.floor(Math.random() * 2)
 
     if (tossResult === 0) {
       this.setState(preState => ({
         result: 'heads',
-        totalCount: preState.totalCount + 1,
         headsCount: preState.headsCount + 1,
+        totalCount: preState.totalCount + 1,
       }))
     } else {
       this.setState(preState => ({
         result: 'tails',
+        tailsCount: preState.tailsCount + 1,
         totalCount: preState.totalCount + 1,
-        headsCount: preState.tailsCount + 1,
       }))
     }
   }
@@ -26,20 +26,20 @@ class CoinToss extends Component {
   render() {
     const {result, headsCount, tailsCount, totalCount} = this.state
 
-    let coinImage
-
-    if (result === 'heads') {
-      coinImage = 'https://assets.ccbp.in/frontend/react-js/heads-img.png'
-    } else {
-      coinImage = 'https://assets.ccbp.in/frontend/react-js/tails-img.png'
-    }
-
     return (
       <div className="container">
         <div className="content-container">
           <h1 className="heading">Coin Toss Game</h1>
           <p className="description">Heads (or) Tails</p>
-          <img src={coinImage} className="coin-image" alt="toss result" />
+          <img
+            src={
+              result === 'heads'
+                ? 'https://assets.ccbp.in/frontend/react-js/heads-img.png'
+                : 'https://assets.ccbp.in/frontend/react-js/tails-img.png'
+            }
+            className="coin-image"
+            alt="toss result"
+          />
           <button className="button" type="button" onClick={this.tossCoin}>
             Toss Coin
           </button>
